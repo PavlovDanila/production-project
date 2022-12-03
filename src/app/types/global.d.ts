4ca549d670347505c76f6fba1c958a067f835c9d
Danila <pavlov.money@gmail.com>
2022-12-03 16:03:41 +0300

    Sidebar

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -5,3 +5,10 @@ interface IClassNames {
 const classNames: IClassNames;
 export = classNames;
 }
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+    const SVG: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    export default SVG;
+}
