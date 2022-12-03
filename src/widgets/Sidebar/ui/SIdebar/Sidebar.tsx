eb2baadcd58b97c143ac8ad07a54b170f70f0414
Danila <pavlov.money@gmail.com>
2022-12-03 18:57:25 +0300

    i18n final

--- src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 
 interface SidebarProps {
   className?: string;
@@ -20,6 +21,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         <Button onClick={onToggle}>toggle</Button>
         <div className={cls.switchers}>
           <ThemeSwitcher />
+          <LangSwitcher className={cls.lang} />
         </div>
     </div>
   );
