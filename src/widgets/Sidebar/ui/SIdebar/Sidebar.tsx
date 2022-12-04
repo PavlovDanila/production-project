559dd76f22559905a55050a4474537057821f75e
Danila <pavlov.money@gmail.com>
2022-12-04 22:57:21 +0300

    started stylelint config

--- src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/SIdebar/Sidebar.tsx
@@ -18,7 +18,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <Button onClick={onToggle}>toggle</Button>
+            <Button onClick={onToggle}>
+                toggle
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
